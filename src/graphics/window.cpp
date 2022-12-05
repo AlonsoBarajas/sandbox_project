1ecb4ab76df5a78205f45f6c04a98f3737b93d72
Neiru <barajasalonsoc@gmail.com>
2022-12-04 19:14:10 -0600

    GLEW succesfully loaded

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -1,4 +1,5 @@
 #include "window.h"
+#include <iostream>
 
 Window::Window(const char * title, int width, int height):
     m_Title(title), m_Width(width), m_Height(height)
@@ -9,23 +10,27 @@ Window::Window(const char * title, int width, int height):
 bool Window::init(){
     
     // initialize GLFW
-    if(!glfwInit()) 
+    if(!glfwInit()){
+std::cerr << "GLFW was not loaded correctly. Exiting ...\n";
         return false;
+    }
 
     m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
 
     // check if the window was properly created
     if(!m_Window){
+std::cerr << "Window was not succesfully created. Exiting ...\n";
         glfwTerminate();
         return false;
     }
 
     glfwMakeContextCurrent(m_Window);
 
-    // initialize GLEW
-    // if(glewInit() != GLEW_OK)
-    //     return false;
-
+    //initialize GLEW
+    if(glewInit() != GLEW_OK){
+std::cerr << "GLEW was not loaded correctly. Exiting ...\n";
+        return false;
+    }
 
     return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
+
 #include "graphics/window.h"
 #include <iostream>
 
 int main (void){
 
-    Window win("Hello World", 640, 480);
+
+    Window win("Hello james", 640, 480);
 
     while (!win.closed()){
         win.update();
     }
+
+
 
     return 0;
 }
