1f28003060c873bc3aad44df43caec8248262982
Neiru <barajasalonsoc@gmail.com>
2022-12-07 12:25:24 -0600

    Multiple attributes in VAO

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,9 @@
 
 int main (void){
 
-    std::string test = read_file("../shaders/basics.frag");
-
     graphx::Window win("Hello james", 640, 480);
 
-    graphx::Shader shader("../shaders/basics.vert", "../shaders/basics.uniform.frag");
+    graphx::Shader shader("../shaders/basics.loccol.vert", "../shaders/basics.loccol.frag");
 
     shader.compile();
 
@@ -18,60 +16,42 @@ int main (void){
     //              Create Vertext Buffer Object VBO
     ///////////////////////////////////////////////////////////////////
 
-    // definition of the triangle vertices
+    // definition of the triangle vertices // Position and color attributes
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,     // lower left corner
-         0.5f, -0.5f, 0.0f,     // lower right corner
-         0.0f,  0.5f, 0.0f      // top
+        -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,     // lower left corner
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // lower right corner
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
     };
 
     // create a vertex buffer object VBO
-    GLuint VBO[2];
-    glGenBuffers(2 , VBO);
+    GLuint VBO;
+    glGenBuffers(1 , &VBO);
 
     // create Vertex Attribute Object
-    GLuint VAO[2];
-    glGenVertexArrays(2, VAO);
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
 
     // bind VAO 
-    glBindVertexArray(VAO[0]);
-
+    glBindVertexArray(VAO);
 
     // bind VBO to the current array buffer
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // this copies the data of the vertices into the currently bound array buffer i.e. VBO
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // tell OpenGL how to interpret the vertex attributes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
 
-    // enables the vertex attribute
+    // position attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+    // color attributes
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
+
+
+    // enables the vertex attributes
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
-    // ..:: Exercise 1.- Draw two triangles next to each other with glDrawArrays
-    // Vertices of both triangles, no Element Buffer Object
-    GLfloat square[18] = {
-        -0.5f, -0.5f, 0.0f,     // bottom left
-         0.5f, -0.5f, 0.0f,     // bottom right
-        -0.5f,  0.5f, 0.0f,     // top left
-
-         0.5f, -0.5f, 0.0f,     // bottom right
-         0.5f,  0.5f, 0.0f,     // top right
-        -0.5f,  0.5f, 0.0f      // top left
-    };
-
-    // bind VBO
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-
-    // bind VAO
-    glBindVertexArray(VAO[1]);
-
-    // copy data into VBO
-    glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(GLfloat), square, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void *)0);
-
-    glEnableVertexAttribArray(0);
 
 
     while (!win.closed()){
@@ -88,7 +68,7 @@ int main (void){
         shader.activate();
         glUniform4f(colorLoc, redChannel, 0.5f, 0.2f, 1.0f);
 
-        glBindVertexArray(VAO[1]);
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
